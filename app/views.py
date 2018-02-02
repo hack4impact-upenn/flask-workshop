2d6c4e3c4d529b0aed73f00b0d126a02f0080480
Katie Jiang <kajiang@seas.upenn.edu>
2018-02-02 02:00:30 -0500

    Initial commit

---
+++ app/views.py
@@ -0,0 +1,13 @@
+from flask import render_template
+from app import app
+
+
+@app.route("/", methods=['GET'])
+def index():
+    return "Hello World"
+    return render_template('index.html')
+
+
+@app.route("/<name>/<int:rating>", methods=['GET'])
+def welcome(name, rating):
+    return render_template('welcome.html', name=name, rating=rating)
