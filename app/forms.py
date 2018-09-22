9c8e8d76abab2ab9290b73903c944d892e57a8c1
Stephanie Shi <stephanie.y.shi@gmail.com>
2018-09-22 01:43:34 -0400

    adding inline comment explanations

--- app/forms.py
+++ app/forms.py
@@ -3,6 +3,7 @@ from wtforms.fields import (IntegerField, StringField, SubmitField)
 from wtforms.validators import InputRequired, Length
 
 
+# Define a form for adding newbies
 class AddNewbieForm(FlaskForm):
     first_name = StringField('First name', validators=[InputRequired(), Length(1, 64)])
     last_name = StringField('Last name', validators=[InputRequired(), Length(1, 64)])
